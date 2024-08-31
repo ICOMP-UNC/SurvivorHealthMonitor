39601cb1ca346c4f189c17d201911fa58ba95b1a
Gabriel Valenzuela <gabriel.valenzuela@wazuh.com>
2024-08-30 22:45:06 -0300

    CL
    - Add missing style and fix action

--- src/main.c
+++ src/main.c
@@ -9,8 +9,7 @@
 
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    printf("Hello World!\n");
-    return 0;
+int main(int argc, char *argv[]) {
+  printf("Hello World!\n");
+  return 0;
 }
