4541e4165fef18357b02dc6338012eae7b870ad3
EmiCasttro01 <emiliano.castro@mi.unc.edu.ar>
2024-09-05 01:43:21 -0300

    documentation3

--- include/console.h
+++ include/console.h
@@ -4,9 +4,13 @@
  * @date 
  */
 
+#pragma once
+#include <stdbool.h>
 #define _STATUS_OK 0
 /**
  * @brief possible console states
+ /**
+ * @brief 
  * 
  */
 typedef enum {
@@ -19,24 +23,48 @@ typedef enum {
 
 
 /**
- * @brief consoleIO
+ * @brief console struct
+ * Has all the necessary information to represent the console.
  * 
- * @param callback 
- * @return int 
+ * 
  */
 
 typedef struct {
-    console_state_t state;
-    char *default_header;
-    char *default_footer;              
-    int (*callback_function)();
-    int console_status;
-    char *console_message;
+    bool state_with_input_data;       // If the state requires input data
+    char* input_data;                 // Input data
+    console_state_t state;            // Current state
+    char *default_header;             // Default header
+    char *default_footer;             // Default footer
+    int (*callback_function)();       // Callback function
+    char* (*input_data_function)();             // Input data function: Needs to be created for each state that requires input data. If state_with_input_data is true, this functions will be called on consoleIO.               
+    int console_status;               // Status of the console
+    char *console_message;            // Message to be printed on the console: If screen needs options to be selected on input_data, this message should include the options.
 } console_t;
-
+/**
+ * @brief Main function for the console 
+ * 
+ * @return int 
+ */
+ 
 int consoleIO(console_t(*)());
+
+/**
+ * @brief Handle states
+ * 
+ */
 void consoleStatesHandler(console_t);
 
+/**
+ * @brief For each console state that needs input data, a function like this should be created.
+ * 
+ */
+char* consolePrintDataInput(); 
+
+/**
+ * @brief All console callbacks functions. 
+ * 
+ * @return console_t 
+ */
 console_t consoleInit();
 console_t consoleExit();
 console_t consoleError();
